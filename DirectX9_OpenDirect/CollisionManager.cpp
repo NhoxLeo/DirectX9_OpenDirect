e4cf10047fc1d5469804b17734553ee2c4b3b397
NhoxLeo <37131285+NhoxLeo@users.noreply.github.com>
2019-07-15 19:16:18 +0700

    Added CollisionManager

---
+++ DirectX9_OpenDirect/CollisionManager.cpp
@@ -0,0 +1,12 @@
+#include "CollisionManager.h"
+
+
+
+CollisionManager::CollisionManager()
+{
+}
+
+
+CollisionManager::~CollisionManager()
+{
+}
---
+++ DirectX9_OpenDirect/CollisionManager.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "stdafx.h"
+class CollisionManager
+{
+public:
+	CollisionManager();
+	~CollisionManager();
+};
+
